122d99486ebd5112072e249b0cb9de5ac03984ad
vehcamut <realshuncle@gmail.com>
2023-07-04 14:11:11 +0400

    main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -179,9 +179,9 @@ const TABS = {
     }]
   }
 };
-// for (let i = 0; i < 6; ++i) {
-//     TABS.all.items = [...TABS.all.items, ...TABS.all.items];
-// }
+for (let i = 0; i < 6; ++i) {
+  TABS.all.items = [...TABS.all.items, ...TABS.all.items];
+}
 const TABS_KEYS = Object.keys(TABS);
 function Main() {
   const ref = React.useRef();
